ae2aa8014d905e4bb64c0942a166d68a18284790
Anna <107642838+Kashitsina@users.noreply.github.com>
2022-07-31 20:07:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿  // Задайте значение M и N.напишите программу ,которая выведет все натуральные числа в промежутке от М до N
+
+
+  int numberN = 4;
+  int numberM = 8;
+
+
+  Recursion(numberN , numberM);
+  Console.WriteLine();
+
+
+  void Recursion(int numberN, int numberM)
+  {
+    if (numberN == numberM +1  ) return ;
+     
+    Console.Write(numberN + "  ");
+    numberN ++;
+    Recursion( numberN, numberM);
+    
+  }
+   
+
